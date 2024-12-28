f4265b31c7a63717f1861393a00c0c7b509d453b
Aleksei Orlov <alexeyorlov65@gmail.com>
2024-12-28 15:45:26 +0300

    admin auth and migrations

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,5 +1,8 @@
 from functools import lru_cache
+from typing import Annotated
+
 from dotenv import load_dotenv
+from fastapi import Depends
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -31,3 +34,20 @@ class RatelimitSettings(BaseSettings):
 @lru_cache
 def get_ratelimit_settings() -> RatelimitSettings:
     return RatelimitSettings()
+
+
+class AdminCredentialsSettings(BaseSettings):
+    login: str
+    password: str
+
+    model_config = SettingsConfigDict(env_prefix="admin_")
+
+
+@lru_cache
+def get_admin_credentials():
+    return AdminCredentialsSettings()
+
+
+AdminCredentialsDep = Annotated[
+    AdminCredentialsSettings, Depends(get_admin_credentials)
+]
